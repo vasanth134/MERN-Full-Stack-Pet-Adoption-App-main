e967798a2b2809766e6725bfd723f3a6622c3185
vasanth134 <vasanth13.g@gmail.com>
2025-03-06 22:05:15 +0530

    Update application title and navbar text to "Find your perfect companion"

--- backend/client/src/components/navBar.tsx
+++ backend/client/src/components/navBar.tsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
               className="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white"
               to="/"
             >
-              petConnectApp
+     Find your perfect companion
             </NavLink>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
